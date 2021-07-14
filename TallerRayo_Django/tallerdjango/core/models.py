b27a412ae300958c863a080fab6e33fa4a6dc2fd
Rodrigo Rivas <rod.rivas@alumnos.duoc.cl>
2021-07-14 13:44:34 -0400

    Inicio de models y views
    Hice el principio de lo que seria models y views para ir separando los datos desde aqui

--- TallerRayo_Django/tallerdjango/core/models.py
+++ TallerRayo_Django/tallerdjango/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.db.models.base import Model
 # Create your models here.
+
+class Perfiltrabajador(models.Model):
+    id_Perfiltrabajador = models.IntegerField(primary_key=True, verbose_name="ID Trabajador")
+    Perfiltrabajador = models.CharField(max_length=60, verbose_name="Perfil del Trabajador")
+    
+    def __str__(self):
+        return self.Perfiltrabajador
+
+class Vehiculo(models.Model):
+    id_Vehiculo = models.IntegerField(primary_key=True, verbose_name="ID Vehiculo")
+    Vehiculo = models.CharField(max_length=60, verbose_name="Vehiculo")
+    
+    def __str__(self):
+        return self.Vehiculo
--- TallerRayo_Django/tallerdjango/core/views.py
+++ TallerRayo_Django/tallerdjango/core/views.py
@@ -2,4 +2,50 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+
     return render(request,'core/home.html')
+
+def formulario(request):
+    return render(request, 'core/formulario.html')
+
+def servicios(request):
+    return render(request, 'core/servicios.html')
+
+def agendar(request):
+    return render(request, 'core/agendar.html')
+
+def cajasdecambio(request):
+    return render(request, 'core/cajasdecambio.html')
+
+def clima(request):
+    return render(request, 'core/clima.html')
+
+def direccion(request):
+    return render(request, 'core/direccion.html')
+
+def electronicaauto(request):
+    return render(request, 'core/electronicaauto')
+
+def geolocalizacion(request):
+    return render(request, 'core/geolocalizacion')    
+
+def inicioperfil(request):
+    return render(request, 'core/inicioperfil')
+
+def mantencion(request):
+    return render(request, 'core/mentencion')
+
+def perfiltrabajador(request):
+    return render(request, 'core/perfiltrabajador')
+
+def quienessomos(request):
+    return render(request, 'core/quienessomos')
+
+def servicios(request):
+    return render(request, 'core/servicios')
+
+def suspension(request):
+    return render(request, 'core/suspension')
+
+def vehiculo(request):
+    return render(request, 'core/vehiculo')
